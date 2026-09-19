fb977d93f071345f9d624e49c23dc72b7ecf70c4
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-19 20:52:29 +0300

    Create media_model.dart

---
+++ lib/models/media_model.dart
@@ -0,0 +1,47 @@
+class MediaModel {
+  final int id;
+  final String title;
+  final String overview;
+  final String? posterPath;
+  final String? backdropPath;
+  final double voteAverage;
+  final String? releaseDate;
+  final String mediaType;
+
+  MediaModel({
+    required this.id,
+    required this.title,
+    required this.overview,
+    this.posterPath,
+    this.backdropPath,
+    required this.voteAverage,
+    this.releaseDate,
+    required this.mediaType,
+  });
+
+  factory MediaModel.fromJson(Map<String, dynamic> json) {
+    return MediaModel(
+      id: json['id'] ?? 0,
+      title: json['title'] ?? json['name'] ?? 'بدون عنوان',
+      overview: json['overview'] ?? '',
+      posterPath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
+      voteAverage: (json['vote_average'] as num?)?.toDouble() ?? 0.0,
+      releaseDate: json['release_date'] ?? json['first_air_date'],
+      mediaType: json['media_type'] ?? 'movie',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'overview': overview,
+      'poster_path': posterPath,
+      'backdrop_path': backdropPath,
+      'vote_average': voteAverage,
+      'release_date': releaseDate,
+      'media_type': mediaType,
+    };
+  }
+}
