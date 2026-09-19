e848b7f769cdf80f75560333ee44f16ffea16829
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-20 01:05:06 +0300

    fix: correct shared TMDB trending URL

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../core/api_config.dart';
+import '../core/constants.dart';
 
 class ApiService {
   static Future<List<dynamic>> getTrendingMedia({String mediaType='all',String timeWindow='day'}) async {
     final key=ApiConfig.tmdbKey;
     if(key.trim().isEmpty) throw StateError('TMDB API key is not configured.');
-    final uri=Uri.parse('\${AppConstants.tmdbBaseUrl}/trending/\$mediaType/\$timeWindow').replace(queryParameters:{'api_key':key});
+    final uri=Uri.parse('${AppConstants.tmdbBaseUrl}/trending/$mediaType/$timeWindow').replace(queryParameters:{'api_key':key});
     final response=await http.get(uri);
     if(response.statusCode!=200) throw Exception('Failed to load media data (\${response.statusCode}).');
     final data=json.decode(utf8.decode(response.bodyBytes));
