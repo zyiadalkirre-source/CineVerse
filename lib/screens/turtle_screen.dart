5593b14c288bb29105c088884f3ae6d3115e20cf
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-20 00:48:44 +0300

    fix: import Turtle LiteRT backend types

--- lib/screens/turtle_screen.dart
+++ lib/screens/turtle_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_litert_lm/flutter_litert_lm.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../services/turtle_brain.dart';
 
