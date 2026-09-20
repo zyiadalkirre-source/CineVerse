d978af1afaa378bc34962623ec8ef46132883267
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-20 20:52:37 +0300

    test: resolve AuthProvider import collision

--- test/auth_provider_test.dart
+++ test/auth_provider_test.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cineverse/providers/auth_provider.dart';
 import 'package:cineverse/services/auth_service.dart';
