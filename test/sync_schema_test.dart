84c7d2104b278e1fa1876e3b8b30a3af80ab77df
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-21 01:41:43 +0300

    test: update schema version expectation for db v6

--- test/sync_schema_test.dart
+++ test/sync_schema_test.dart
@@ -3,7 +3,7 @@ import 'package:cineverse/core/constants.dart';
 
 void main() {
   test('sync schema constants are versioned and stable', () {
-    expect(AppConstants.dbVersion, 3);
+    expect(AppConstants.dbVersion, 6);
     expect(AppConstants.tableSyncOutbox, 'sync_outbox');
     expect(AppConstants.tableSyncState, 'sync_state');
   });
