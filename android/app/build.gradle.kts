30c84f3bd444d90ffa148d00a94136744ce9c410
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-21 22:06:40 +0300

    Raise Android compile SDK to API 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.cineverse.cineverse"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
