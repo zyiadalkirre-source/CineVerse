0de814112138421d76757e6021a14ceb2ec5e598
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-21 19:52:58 +0300

    fix(auth): disambiguate AuthProvider in account screen

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:provider/provider.dart';
 
 import '../providers/auth_provider.dart';
