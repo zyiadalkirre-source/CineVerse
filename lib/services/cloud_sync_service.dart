d52c4b1ebfc424987825dadfb38844545358f28e
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-21 19:26:03 +0300

    fix(sync): import Flutter diagnostics explicitly

--- lib/services/cloud_sync_service.dart
+++ lib/services/cloud_sync_service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sqflite/sqflite.dart';
